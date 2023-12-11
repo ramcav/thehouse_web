31d0450894029791ef1d8e6443eee465b36a9b5b
ramcav <110005362+ramcav@users.noreply.github.com>
2023-12-12 00:41:51 +0100

    Initial version

--- theHouse/waitlist/urls.py
+++ theHouse/waitlist/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "thehouse"
 urlpatterns = [
     path('', views.LandingView.as_view(), name="landing"),
-    path('waitlist/', views.LandingView.as_view(), name="waitlist")
+    path('success/', views.Confirmation.as_view(), name="waitlist")
 ]
--- theHouse/waitlist/views.py
+++ theHouse/waitlist/views.py
@@ -1,26 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views import View
 from .forms import UserSubmissionForm
+from .models import UserSubmission
 
 class LandingView(View):
     def get(self, request, *args, **kwargs):
         form = UserSubmissionForm()
-        return render(request, 'website/landing.html', {'form': form})
+        return render(request, 'waitlist/landing.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = UserSubmissionForm(request.POST)
         if form.is_valid():
-            # Save the form and do something specific for 'waitlist'
+            # Extract the username from the form
+            username = form.cleaned_data.get('username')
+            email = form.cleand_data.get('email')
+
+            # Check if a user with this username already exists
+            if UserSubmission.objects.filter(username=username).exists():
+                # Handle the case where the username already exists
+                # You can add an error message to the form or context here
+                return render(request, 'waitlist/landing.html', {'form': form, 'error': 'Username already exists.'})
+            elif UserSubmission.objects.filter(email=email).exists():
+                return render(request, 'waitlist/landing.html', {'form': form, 'error': 'Username already exists.'})
+
+            # Save the form since the username is unique
             submission = form.save(commit=False)
-            # You can add additional processing here if necessary
+            # Additional processing if necessary
             submission.save()
-            # Redirect to the same page with a success message
-            return render(request, 'website/landing.html', {
-                'form': UserSubmissionForm(),  # Provide a fresh form
-                'success': True  # Add success flag for the popup message
-            })
+
+            # Redirect to success page
+            return redirect("/success")
         else:
-            # If the form is not valid, render the page with the form errors
-            return render(request, 'website/landing.html', {'form': form})
+            # Form is not valid, render the page with form errors
+            return render(request, 'waitlist/landing.html', {'form': form})
+        
+class Confirmation(View):
+    def get(self, request):
+        return render(request, 'waitlist/confirmation.html')
             
